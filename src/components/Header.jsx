3d2d842ac423c6704789cfc6f9f4e2d6b330736b
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-11-27 11:30:20 -0300

    FEAT: Header stylized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../styles/Header.css';
 
 class Header extends React.Component {
   render() {
-    return <header><h1>Sistema Solar</h1></header>;
+    return <header id='header'><h1>Sistema Solar</h1></header>;
   }
 }
 
